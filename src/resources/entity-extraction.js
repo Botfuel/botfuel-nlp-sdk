6a35d9b46858c25a04ac7db75fe91663b097d620
Maxime Khoy <maxime.khoy@ionisx.com>
2017-07-18 14:30:00 +0200

    Add missing parameters & useQuerystring option for arrays

--- src/resources/entity-extraction.js
+++ src/resources/entity-extraction.js
@@ -2,15 +2,37 @@ import rp from 'request-promise-native';
 import config from '../config';
 import ApiResource from './api-resource';
 
-
 export default class EntityExtraction extends ApiResource {
-
-  compute({ sentence, locale, entities }) {
+  compute({
+    sentence,
+    locale,
+    dimensions,
+    antidimensions,
+    timezone,
+    capitalize,
+    quote,
+    unicode,
+    latent,
+  }) {
     const options = {
       method: 'GET',
       rejectUnauthorized: false,
       uri: config.ENTITY_EXTRACTION_API,
-      qs: { sentence, locale, entities },
+      // Needed so that arrays are serialized to foo=bar&foo=baz
+      // Instead of foo[0]=bar&foo[1]=baz
+      // (dimensions for example)
+      useQuerystring: true,
+      qs: {
+        sentence,
+        locale,
+        dimensions,
+        antidimensions,
+        timezone,
+        cap: capitalize,
+        quo: quote,
+        uni: unicode,
+        latent,
+      },
       headers: this.headers,
     };
     return rp(options);
