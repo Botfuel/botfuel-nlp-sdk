c79ca231ef3f9a7c9d1fd48881d6526cc7518261
Maxime Khoy <maxime.khoy@ionisx.com>
2017-07-18 14:57:44 +0200

    Rename headers to App-Id & App-Key

--- src/resources/api-resource.js
+++ src/resources/api-resource.js
@@ -1,6 +1,5 @@
-
 export default class ApiResource {
   constructor({ appId, appKey }) {
-    this.headers = { app_id: appId, app_key: appKey };
+    this.headers = { 'App-Id': appId, 'App-Key': appKey };
   }
 }
